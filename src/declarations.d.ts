dd889ef7f87126011f88526aaac287dbb53e54ff
mikesplore <mikepremium8@gmail.com>
2025-05-23 13:56:55 +0300

    update

---
+++ src/declarations.d.ts
@@ -0,0 +1,9 @@
+declare module '*.jsx' {
+  import React from 'react'
+  const component: React.ComponentType<any>
+  export default component
+}
+
+declare module '*.jpg'
+declare module '*.png'
+declare module '*.svg'
